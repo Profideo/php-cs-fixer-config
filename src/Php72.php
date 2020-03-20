c76f598218920aa9f20e85fc4166d7f474998db8
bmehdi <ebrahimi@profideo.com>
2020-03-20 15:53:58 +0100

    Prefix native PHP function call with \

--- src/Php72.php
+++ src/Php72.php
@@ -9,6 +9,12 @@ final class Php72 extends Config
     public function __construct()
     {
         parent::__construct('Profideo (PHP 7.2)');
+
+        $this->setRiskyAllowed(true);
+        // We can allow risky because we add only native_function_invocation risky rule and our applications
+        // do not override native functions.
+        // We cannot allow risky for only this rule for the moment. See
+        // https://github.com/FriendsOfPHP/PHP-CS-Fixer/issues/4653.
     }
 
     public function getRules()
@@ -18,7 +24,8 @@ final class Php72 extends Config
             'array_syntax' => [
                 'syntax' => 'short',
             ],
-            'single_line_throw' => false
+            'single_line_throw' => false,
+            'native_function_invocation' => true,
         ];
 
         return $rules;
